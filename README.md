27890b1ee1ac450e83f246dd99beb861f75d2299
Irfan Maulana <irfan.maulana@gdn-commerce.com>
2017-05-24 11:01:48 +0700

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,72 @@
 # blibli-login-helper
 Blibli Login-Register Validation Helper
+
+[![License](https://img.shields.io/github/license/mazipan/blibli-login-helper.svg?maxAge=3600)](https://github.com/mazipan/blibli-login-helper) 
+[![Github Issue](https://img.shields.io/github/issues/mazipan/blibli-login-helper.svg?maxAge=3600)](https://github.com/mazipan/blibli-login-helper/issues) 
+[![GitHub Fork](https://img.shields.io/github/forks/mazipan/blibli-login-helper.svg?maxAge=3600)](https://github.com/mazipan/blibli-login-helper/network) 
+[![GitHub Star](https://img.shields.io/github/stars/mazipan/blibli-login-helper.svg?maxAge=3600)](https://github.com/mazipan/blibli-login-helper/stargazers) 
+[![GitHub Contributors](https://img.shields.io/github/contributors/mazipan/blibli-login-helper.svg?maxAge=3600)](https://github.com/mazipan/blibli-login-helper/network/members) 
+
+[![version](https://img.shields.io/npm/v/blibli-login-helper.svg?maxAge=3600)](https://www.npmjs.com/package/blibli-login-helper)
+[![downloads](https://img.shields.io/npm/dt/blibli-login-helper.svg?maxAge=3600)](https://www.npmjs.com/package/blibli-login-helper) 
+
+## Available Methods
+
+```
+/**
+ * Validating email format
+ * @param {string} email string
+ * @return {boolean} true if valid
+ */
+ 
+ validateEmail(string)
+```
+
+
+```
+/**
+ * Validating password format
+ * @param {string} password string
+ * @return {string} error code when not valid
+ */
+
+validatePassword(string) 
+```
+
+
+```
+/**
+ * Handle error code in login/register from backend
+ * @param {Object} response body from backend
+ * @param {Object} self/this from component
+ * @return {Object} error object that assign to error data
+ */
+
+failedHandler(response, self)
+```
+
+
+```
+/**
+ * Initialize facebook SDK
+ * @param {string} facebookAppId
+ */
+
+FBinitialize(facebookAppId)
+```
+
+
+```
+
+/**
+ * Login with Facebook SDK
+ * @param {Function} submitFn
+ */
+
+loginFacebook(submitFn)
+```
+
+
+Hope will usefull for you all.
+
+Question ? please email : mazipanneh@gmail.com
---
+++ login-helper.js
@@ -0,0 +1,131 @@
+/*!
+ *  @author: Irfan Maulana 
+ *  https://github.com/mazipan/blibli-login-helper  
+ * 
+ */
+
+
+/* eslint-disable no-useless-escape */
+/**
+ * Validating email format
+ * @param {string} email string
+ * @return {boolean} true if valid
+ */
+module.exports.validateEmail = function (string) {
+  const REGEX_EMAIL = /^(([^()[\]\\.,;:\s@\"]+(\.[^()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  return REGEX_EMAIL.test(string)
+}
+
+/* eslint-disable no-useless-escape */
+/**
+ * Validating password format
+ * @param {string} password string
+ * @return {string} error code when not valid
+ */
+module.exports.validatePassword = function (string) {
+  let errorCode = ''
+  if (typeof string === 'undefined' || string === null) {
+    errorCode = 'INVALID_BLANK'
+  } else if (string.length < 6 || string.search(/\d/) === -1 || string.search(/[a-zA-Z]/) === -1) {
+    errorCode = 'INVALID_PASSWORD'
+  } else if (string.indexOf('%') !== -1) {
+    errorCode = 'INVALID_CHAR_PERCENT'
+  } else if (string.indexOf('=') !== -1) {
+    errorCode = 'INVALID_CHAR_ASSIGNMENT'
+  }
+
+  return errorCode
+}
+
+/**
+ * Handle error code in login/register from backend
+ * @param {Object} response body from backend
+ * @param {Object} self/this from component
+ * @return {Object} error object that assign to error data
+ */
+module.exports.failedHandler = function (response, self) {
+  let errorObject = { general: { error: true, code: response.status, text: self.$t('error_login.UNKNOWN') } }
+  if (response) {
+    if (response.code === 401) {
+      errorObject = { general: { error: true, code: response.status, text: self.$t('error_login.UNAUTHORIZED') } }
+    } else if (response.code === 400) {
+      let multipleError = ''
+      for (let key in response.errors) {
+        response.errors[key].map(elem => {
+          if (multipleError !== '') {
+            multipleError += ' <br/> '
+          }
+          let errorStringValue = self.$t('error_login.' + elem)
+          if (errorStringValue !== null) {
+            let cleanError = errorStringValue.replace('[INPUT_VALUE]', key)
+            multipleError += cleanError
+          } else {
+            multipleError = self.$t('error_login.UNKNOWN')
+          } return elem
+        })
+      }
+      errorObject = { general: { error: true, code: 'MULTIPLE_ERROR', text: multipleError } }
+    }
+  }
+  return errorObject
+}
+
+/**
+ * Initialize facebook SDK
+ * @param {string} facebookAppId
+ */
+module.exports.FBinitialize = function (facebookAppId) {
+  (function (d, s, id) {
+    var js = {}
+    var fjs = d.getElementsByTagName(s)[0]
+    if (d.getElementById(id)) return
+    js = d.createElement(s)
+    js.id = id
+    js.async = true
+    js.defer = true
+    js.src = '//connect.facebook.net/en_US/sdk.js'
+    fjs.parentNode.insertBefore(js, fjs)
+  }(document, 'script', 'facebook-jssdk'))
+
+  /* eslint-disable no-undef */
+  window.fbAsyncInit = function () {
+    if (typeof FB !== 'undefined') {
+      FB.init({
+        appId: facebookAppId,
+        cookie: true,
+        xfbml: true,
+        version: 'v2.7'
+      })
+    }
+  }
+}
+
+/**
+ * Login with Facebook SDK
+ * @param {Function} submitFn
+ */
+module.exports.loginFacebook = function (submitFn) {
+  /* eslint-disable no-undef */
+  const urlApi = '/me?fields=gender,first_name,last_name,birthday,locale,timezone,email'
+  let fetchFacebookData = (accessToken) => {
+    if (typeof FB !== 'undefined') {
+      FB.api(urlApi, function (response) {
+        submitFn(response, accessToken)
+      })
+    }
+  }
+
+  if (typeof FB !== 'undefined') {
+    FB.getLoginStatus(function (response) {
+      if (response.status === 'connected') {
+        fetchFacebookData(response.authResponse.accessToken)
+      } else {
+        FB.login(function (response) {
+          if (response.status === 'connected') {
+            fetchFacebookData(response.authResponse.accessToken)
+          }
+        }, {scope: 'public_profile,email'})
+      }
+    })
+  }
+}

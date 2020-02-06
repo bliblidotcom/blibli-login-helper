97bc9bcca89ccdb6bcc6d5156a8008e39421f940
hidayat.febiansyah <hidayat.febiansyah@gdn-commerce.com>
2020-02-06 15:58:31 +0700

    update validate password by allowing '%' and '=' characters

---
+++ tests/_validatePassword.spec.js
@@ -0,0 +1,21 @@
+const obj = require('../module/_validatePassword')
+
+describe('_validatePassword', () => {
+  test('Blank password', () => {
+    expect(obj._validatePassword()).toBe('INVALID_BLANK')
+    expect(obj._validatePassword('')).toBe('INVALID_BLANK')
+    expect(obj._validatePassword('   ')).toBe('INVALID_BLANK')
+  })
+
+  test('Invalid password', () => {
+    expect(obj._validatePassword('dfadd')).toBe('INVALID_PASSWORD')
+    expect(obj._validatePassword('33333')).toBe('INVALID_PASSWORD')
+    expect(obj._validatePassword('ee333')).toBe('INVALID_PASSWORD')
+  })
+
+  test('Valid password', () => {
+    expect(obj._validatePassword('ddfd33')).toBe('')
+    expect(obj._validatePassword('dafdsa3dfasfa')).toBe('')
+    expect(obj._validatePassword('fdsfsasa33')).toBe('')
+  })
+})
